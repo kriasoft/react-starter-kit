52fb37cc29434fe915f9ba21c8158c12bcd091cc
Vishwanath Arondekar <vishwanatharondekar@gmail.com>
2014-07-15 03:32:02 +0400

    Update node.js modules

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,10 @@
  * Copyright (c) KriaSoft, LLC. All rights reserved. See LICENSE.txt
  */
 
-/*
- * For more information on how to configure Gulp, please visit:
- * https://github.com/gulpjs/gulp/blob/master/docs/API.md
- */
+'use strict';
 
-// Include Gulp & other tools for build automation
+// Include Gulp and other build automation tools and utilities
+// See: https://github.com/gulpjs/gulp/blob/master/docs/API.md
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var es = require('event-stream');
@@ -18,9 +16,10 @@ var webpack = require('webpack');
 var argv = require('minimist')(process.argv.slice(2));
 
 // Settings
+var DEST = './build';
 var DEBUG = !argv.release;
-var WATCH = Boolean(argv.watch);
-var LOG = Boolean(argv.log);
+var WATCH = !!argv.watch;
+var LOG = !!argv.log;
 
 // Node.js runtime dependencies and their version numbers
 var pkgs = require('./package.json').dependencies;
@@ -30,7 +29,7 @@ Object.keys(pkgs).forEach(function (key) { return pkgs[key] = pkgs[key].substrin
 // -----------------------------------------------------------------------------
 gulp.task('clean', function (cb) {
     var rimraf = require('rimraf');
-    rimraf('./build', cb);
+    rimraf(DEST, cb);
 });
 
 // Copy vendor files
@@ -38,9 +37,9 @@ gulp.task('clean', function (cb) {
 gulp.task('vendor', function () {
     return es.merge(
         gulp.src('./node_modules/jquery/dist/**')
-            .pipe(gulp.dest('./build/vendor/jquery-' + pkgs.jquery)),
+            .pipe(gulp.dest(DEST + '/vendor/jquery-' + pkgs.jquery)),
         gulp.src('./node_modules/bootstrap/dist/fonts/**')
-            .pipe(gulp.dest('./build/fonts'))
+            .pipe(gulp.dest(DEST + '/fonts'))
     );
 });
 
@@ -49,9 +48,9 @@ gulp.task('vendor', function () {
 gulp.task('assets', function () {
     return es.merge(
         gulp.src('./src/assets/**')
-            .pipe(gulp.dest('./build')),
+            .pipe(gulp.dest(DEST)),
         gulp.src('./src/images/**')
-            .pipe(gulp.dest('./build/images/')),
+            .pipe(gulp.dest(DEST + '/images/')),
         gulp.src('./src/*.html')
             .pipe(DEBUG ? $.util.noop() : $.htmlmin({
                 removeComments: true,
@@ -59,7 +58,7 @@ gulp.task('assets', function () {
                 minifyJS: true
             }))
             .pipe(DEBUG ? $.embedlr() : $.util.noop())
-            .pipe(gulp.dest('./build'))
+            .pipe(gulp.dest(DEST))
     );
 });
 
@@ -71,7 +70,7 @@ gulp.task('styles', function () {
         .pipe($.less({sourceMap: DEBUG, sourceMapBasepath: __dirname}))
         .on('error', $.util.log)
         .pipe(DEBUG ? $.util.noop() : $.minifyCss())
-        .pipe(gulp.dest('./build/css'));
+        .pipe(gulp.dest(DEST + '/css'));
 });
 
 // Create JavaScript bundle
@@ -112,15 +111,15 @@ gulp.task('serve', ['build'], function (next) {
             // For non-existent files output the contents of /index.html page in order to make HTML5 routing work
             var urlPath = url.parse(req.url).pathname;
             if (urlPath === '/') {
-                req.url = '/build/index.html';
+                req.url = DEST.substring(1) + '/index.html';
             } else if (['src', 'bower_components'].indexOf(urlPath.split('/')[1]) === -1) {
                 if (urlPath.length > 3 &&
                     ['src', 'bower_components'].indexOf(urlPath.split('/')[1]) === -1 &&
                     ['css', 'html', 'ico', 'js', 'png', 'txt', 'xml'].indexOf(urlPath.split('.').pop()) == -1 &&
                     ['fonts', 'images', 'vendor', 'views'].indexOf(urlPath.split('/')[1]) == -1) {
-                    req.url = '/build/index.html';
+                    req.url = DEST.substring(1) + '/index.html';
                 } else {
-                    req.url = '/build' + req.url;
+                    req.url = DEST.substring(1) + req.url;
                 }
             }
             server(req, res);
@@ -142,8 +141,8 @@ gulp.task('watch', ['serve'], function () {
     gulp.watch('./src/**/*.less', ['styles']);
 
     // Watch for changes in 'compiled' files
-    gulp.watch('./build/**', function (file) {
-        var relPath = 'build\\' + path.relative('./build', file.path);
+    gulp.watch(DEST + '/**', function (file) {
+        var relPath = DEST.substring(2) + '\\' + path.relative(DEST, file.path);
         $.util.log('File changed: ' + $.util.colors.magenta(relPath));
         lr.changed(file.path);
     });
@@ -156,7 +155,7 @@ gulp.task('watch', ['serve'], function () {
 gulp.task('deploy', ['build'], function (cb) {
     var url = 'https://github.com/{name}/{name}.github.io.git';
     var exec = require('child_process').exec;
-    var cwd = path.join(__dirname, './build');
+    var cwd = path.join(__dirname, DEST);
     var cmd = 'git init && git remote add origin ' + url + ' && ' +
               'git add . && git commit -m Release && ' +
               'git push -f origin master';
