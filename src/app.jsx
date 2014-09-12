88beedac7595e15dfe2b3907d56344a99309211e
Vishwanath Arondekar <vishwanatharondekar@gmail.com>
2014-09-13 01:20:08 +0400

    Add routes, pages, components, layouts

--- src/app.jsx
+++ src/app.jsx
@@ -3,41 +3,21 @@
  */
 
 var React = require('react');
+var Router = require('react-router');
 
-var Navbar = React.createClass({
-    render: function () {
-        return (
-            <div className="navbar navbar-inverse navbar-fixed-top">
-                <div className="container">
-                    <a className="navbar-brand" href="/"><img src="/images/logo-small.png" width="38" height="38" alt="" /> React Seed</a>
-                </div>
-            </div>
-        );
-    }
-});
+var Routes = Router.Routes;
+var Route = Router.Route;
 
-var Page = React.createClass({
-    render: function () {
-        return (
-            <div className="container">
-                <h2>Facebook React Starter Kit</h2>
-                <p>This is a single-page application (SPA) project template based on Facebook React.</p>
-                <h4>Runtime Components:</h4>
-                <ul>
-                    <li><a href="https://facebook.github.io/react/">React</a> - A JavaScript library for building user interfaces, developed by Facebook</li>
-                    <li><a href="http://getbootstrap.com/">Bootstrap</a> - CSS framework for developing responsive, mobile first interfaces</li>
-                    <li><a href="http://jquery.com/">jQuery</a> - a JavaScript library designed to simplify the client-side scripting of HTML</li>
-                </ul>
-                <h4>Development Tools:</h4>
-                <ul>
-                    <li><a href="http://webpack.github.io/">Webpack</a> - Compiles front-end source code into modules / bundles</li>
-                    <li><a href="http://gulpjs.com">Gulp</a> - JavaScript streaming build system and task automation</li>
-                </ul>
-                <h3>Fork me on GitHub</h3>
-                <p><a href="https://github.com/kriasoft/react-seed">https://github.com/kriasoft/react-seed</a></p>
-            </div>
-        );
-    }
-});
+var DefaultLayout = require('./layouts/Default.jsx');
+var HomePage = require('./pages/Home.jsx');
+var PrivacyPage = require('./pages/Privacy.jsx');
 
-React.renderComponent(<div><Navbar /><Page /></div>, document.body);
+React.renderComponent(
+    <Routes location="history">
+        <Route name="app" path="/" handler={DefaultLayout}>
+            <Route name="home" path="/" handler={HomePage} />
+            <Route name="privacy" path="/privacy" handler={PrivacyPage} />
+        </Route>
+    </Routes>,
+    document.body
+);
---
+++ src/layouts/Default.jsx
@@ -0,0 +1,23 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require('react');
+var Link = require('react-router').Link;
+var Navbar = require('../components/Navbar.jsx');
+
+module.exports = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Navbar />
+                <this.props.activeRouteHandler />
+                <div className="navbar navbar-fixed-bottom">
+                    <div className="container">
+                        <p className="text-muted">&copy; KriaSoft • <Link to="home">Home</Link> • <Link to="privacy">Privacy</Link></p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
