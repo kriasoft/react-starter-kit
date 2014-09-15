5df666ddb9e699ddea7d9e174f7e6ba26efc03ea
Konstantin Tarkus <hello@tarkus.me>
2014-09-15 20:28:40 +0400

    Fix a typo in webpack.config.js. Resolves #8

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -52,15 +52,15 @@ module.exports = function (release) {
                 },
                 {
                     test: /\.gif/,
-                    loader: 'url-loader?limit=10000&minetype=image/gif'
+                    loader: 'url-loader?limit=10000&mimetype=image/gif'
                 },
                 {
                     test: /\.jpg/,
-                    loader: 'url-loader?limit=10000&minetype=image/jpg'
+                    loader: 'url-loader?limit=10000&mimetype=image/jpg'
                 },
                 {
                     test: /\.png/,
-                    loader: 'url-loader?limit=10000&minetype=image/png'
+                    loader: 'url-loader?limit=10000&mimetype=image/png'
                 },
                 {
                     test: /\.jsx$/,
