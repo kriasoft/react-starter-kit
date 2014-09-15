8f82cbae5c306b9d7f0428c667faca7fd3b6d525
Konstantin Tarkus <hello@tarkus.me>
2014-09-15 22:58:34 +0400

    Replace 4-spaces tabs with 2-spaces

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -11,62 +11,62 @@
 var webpack = require('webpack');
 
 module.exports = function (release) {
-    return {
-        output: {
-            publicPatch: './build/',
-            path: './build/',
-            filename: 'app.js'
-        },
+  return {
+    output: {
+      publicPatch: './build/',
+      path: './build/',
+      filename: 'app.js'
+    },
 
-        cache: !release,
-        debug: !release,
-        devtool: false,
-        entry: './src/app.jsx',
+    cache: !release,
+    debug: !release,
+    devtool: false,
+    entry: './src/app.jsx',
 
-        stats: {
-            colors: true,
-            reasons: !release
-        },
+    stats: {
+      colors: true,
+      reasons: !release
+    },
 
-        plugins: release ? [
-            new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
-            new webpack.optimize.DedupePlugin(),
-            new webpack.optimize.UglifyJsPlugin(),
-            new webpack.optimize.OccurenceOrderPlugin(),
-            new webpack.optimize.AggressiveMergingPlugin()
-        ] : [],
+    plugins: release ? [
+      new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.AggressiveMergingPlugin()
+    ] : [],
 
-        module: {
-            preLoaders: [
-                {
-                    test: '\\.js$',
-                    exclude: 'node_modules',
-                    loader: 'jshint'
-                }
-            ],
-
-            loaders: [
-                {
-                    test: /\.css$/,
-                    loader: 'style!css'
-                },
-                {
-                    test: /\.gif/,
-                    loader: 'url-loader?limit=10000&mimetype=image/gif'
-                },
-                {
-                    test: /\.jpg/,
-                    loader: 'url-loader?limit=10000&mimetype=image/jpg'
-                },
-                {
-                    test: /\.png/,
-                    loader: 'url-loader?limit=10000&mimetype=image/png'
-                },
-                {
-                    test: /\.jsx$/,
-                    loader: 'jsx-loader'
-                }
-            ]
+    module: {
+      preLoaders: [
+        {
+          test: '\\.js$',
+          exclude: 'node_modules',
+          loader: 'jshint'
         }
-    };
+      ],
+
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: 'style!css'
+        },
+        {
+          test: /\.gif/,
+          loader: 'url-loader?limit=10000&mimetype=image/gif'
+        },
+        {
+          test: /\.jpg/,
+          loader: 'url-loader?limit=10000&mimetype=image/jpg'
+        },
+        {
+          test: /\.png/,
+          loader: 'url-loader?limit=10000&mimetype=image/png'
+        },
+        {
+          test: /\.jsx$/,
+          loader: 'jsx-loader'
+        }
+      ]
+    }
+  };
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,27 +7,27 @@
 var React = require('react');
 
 var HomePage = React.createClass({
-    render: function () {
-        return (
-            <div className="container">
-                <h2>Facebook React Starter Kit</h2>
-                <p>This is a single-page application (SPA) project template based on Facebook React.</p>
-                <h4>Runtime Components:</h4>
-                <ul>
-                    <li><a href="https://facebook.github.io/react/">React</a> - A JavaScript library for building user interfaces, developed by Facebook</li>
-                    <li><a href="https://github.com/rackt/react-router">React-Router</a> - A complete routing library for React</li>
-                    <li><a href="http://getbootstrap.com/">Bootstrap</a> - CSS framework for developing responsive, mobile first interfaces</li>
-                </ul>
-                <h4>Development Tools:</h4>
-                <ul>
-                    <li><a href="http://webpack.github.io/">Webpack</a> - Compiles front-end source code into modules / bundles</li>
-                    <li><a href="http://gulpjs.com">Gulp</a> - JavaScript streaming build system and task automation</li>
-                </ul>
-                <h3>Fork me on GitHub</h3>
-                <p><a href="https://github.com/kriasoft/react-seed">https://github.com/kriasoft/react-seed</a></p>
-            </div>
-        );
-    }
+  render: function () {
+    return (
+      <div className="container">
+        <h2>Facebook React Starter Kit</h2>
+        <p>This is a single-page application (SPA) project template based on Facebook React.</p>
+        <h4>Runtime Components:</h4>
+        <ul>
+          <li><a href="https://facebook.github.io/react/">React</a> - A JavaScript library for building user interfaces, developed by Facebook</li>
+          <li><a href="https://github.com/rackt/react-router">React-Router</a> - A complete routing library for React</li>
+          <li><a href="http://getbootstrap.com/">Bootstrap</a> - CSS framework for developing responsive, mobile first interfaces</li>
+        </ul>
+        <h4>Development Tools:</h4>
+        <ul>
+          <li><a href="http://webpack.github.io/">Webpack</a> - Compiles front-end source code into modules / bundles</li>
+          <li><a href="http://gulpjs.com">Gulp</a> - JavaScript streaming build system and task automation</li>
+        </ul>
+        <h3>Fork me on GitHub</h3>
+        <p><a href="https://github.com/kriasoft/react-seed">https://github.com/kriasoft/react-seed</a></p>
+      </div>
+    );
+  }
 });
 
 module.exports = HomePage;
