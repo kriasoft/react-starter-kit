c3c067e7af077ae00d948d579f7e714406d6f937
Konstantin Tarkus <hello@tarkus.me>
2014-09-17 17:55:28 +0400

    Add a unit test sample with Karma

---
+++ config/karma.js
@@ -0,0 +1,44 @@
+var webpackConfig = require('./webpack.js')(/* release */ false);
+
+module.exports = function (config) {
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'src/**/*Spec.jsx'
+    ],
+
+    preprocessors: {
+      'src/**/*Spec.jsx': ['webpack']
+    },
+
+    webpack: {
+      cache: true,
+      module: {
+        loaders: webpackConfig.module.loaders
+      }
+    },
+
+    webpackServer: {
+      stats: {
+        colors: true
+      }
+    },
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
+      'karma-jasmine',
+      'karma-webpack'
+    ]
+
+  });
+};
