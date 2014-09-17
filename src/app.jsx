83873306a4683ace08c74a0d3c6db6b4972bb6cf
Vishwanath Arondekar <vishwanatharondekar@gmail.com>
2014-09-17 10:40:46 +0400

    Add support of React Dev Tools

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,9 @@
 var React = require('react');
 var {Routes, Route} = require('react-router');
 
+// Export React so the devtools can find it
+(window !== window.top ? window.top : window).React = React;
+
 React.renderComponent(
   <Routes location="history">
     <Route name="app" path="/" handler={require('./layouts/Default')}>
