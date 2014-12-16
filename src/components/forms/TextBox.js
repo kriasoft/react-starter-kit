90b0b460fd8a060760e7ec0ba953cd3d927e0e04
Konstantin Tarkus <hello@tarkus.me>
2014-12-16 10:55:36 +0300

    Add a sample TextBox component

---
+++ src/components/forms/TextBox.js
@@ -0,0 +1,40 @@
+/*
+ * React.js Starter Kit
+ * Copyright (c) 2014 Konstantin Tarkus (@koistya), KriaSoft LLC.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+'use strict';
+
+require('./TextBox.less');
+
+var React = require('react');
+
+var TextBox = React.createClass({
+
+  propTypes: {
+    maxLines: React.PropTypes.number
+  },
+
+  getDefaultProps() {
+    return {
+      maxLines: 1
+    };
+  },
+
+  render() {
+    return (
+      <div className="TextBox">
+        {this.props.maxLines > 1 ?
+          <textarea {...this.props} className="TextBox-input" ref="input" key="input" rows={this.props.maxLines} /> :
+          <input {...this.props} className="TextBox-input" ref="input" key="input" />}
+      </div>
+    );
+  }
+
+});
+
+module.exports = TextBox;
+
