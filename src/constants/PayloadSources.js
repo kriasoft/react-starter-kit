ba4f56f1336813bb11f8bba013f7d02040228928
Vishwanath Arondekar <vishwanatharondekar@gmail.com>
2014-12-22 09:15:03 +0300

    Fix code style in PR #45

--- src/constants/PayloadSources.js
+++ src/constants/PayloadSources.js
@@ -5,10 +5,16 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
+
+'use strict';
+
 var keyMirror = require('react/lib/keyMirror');
-module.exports = keyMirror({
+
+var PayloadSources = keyMirror({
 
   VIEW_ACTION: null,
   SERVER_ACTION: null
 
 });
+
+module.exports = PayloadSources;
