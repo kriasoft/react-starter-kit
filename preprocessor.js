2a7587fec9e7ee8e041896b491cf19ed0a8ce5de
Konstantin Tarkus <hello@tarkus.me>
2015-02-09 01:35:28 +0300

    Update Jest configuration

--- preprocessor.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var ReactTools = require('react-tools');
-
-module.exports = {
-  process: function(src, path) {
-    return ReactTools.transform(src, { harmony: true, stripTypes: true });
-  }
-};
