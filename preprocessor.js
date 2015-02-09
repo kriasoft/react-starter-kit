a3505d7bb42ac3afa5ad9c28b05bee75b44ff3f4
Konstantin Tarkus <hello@tarkus.me>
2015-02-09 22:58:30 +0300

    Update `./preprocessor.js`. Ref #64

--- preprocessor.js
+++ preprocessor.js
@@ -4,14 +4,12 @@ var to5 = require('6to5-core');
 
 module.exports = {
   process: function(src, filename) {
-    // Ignore files other than *.js and *.jsx
-    var ext = filename.split('.').pop();
-    if (!/\.jsx?$/i.test(filename)) {
+    // Ignore files other than .js, .es, .jsx or .es6
+    if (!to5.canCompile(filename)) {
       return '';
     }
     // Ignore all files within node_modules
-    // 6to5 files can be .js, .es, .jsx or .es6
-    if (filename.indexOf('node_modules') === -1 && to5.canCompile(filename)) {
+    if (filename.indexOf('node_modules') === -1) {
       return to5.transform(src, { filename: filename }).code;
     }
     return src;
