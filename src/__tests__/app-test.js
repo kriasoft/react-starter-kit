ce3b78642569de1772ab6947a8f8a9ed916d87a0
Dave Hendler <sollaires@gmail.com>
2015-05-27 11:03:25 -0700

    Fix jest linting, adds basic test
    ESLint didn't like Jest globals.  I added an ESLint config extension that allows Jest's
    globals so that `npm test` didn't fail on the eslint run.

---
+++ src/__tests__/app-test.js
@@ -0,0 +1,7 @@
+// As simple as it gets.  This just verifies that `npm test` works using Jest
+
+describe('RSK Jest Integration', function() {
+  it('works', function() {
+    expect(true).toBe(true);
+  });
+});
