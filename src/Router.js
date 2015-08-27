5d92fe358847b4545076ecce7ae4946aa3e1ec3c
Vishwanath Arondekar <vishwanatharondekar@gmail.com>
2015-08-27 05:03:11 +0300

    Update npm modules
    flux 2.0.3 -> 2.1.1: https://github.com/facebook/flux/blob/master/CHANGELOG.md
    babel-eslint 4.0.5 -> 4.1.0: https://github.com/babel/babel-eslint/commits/master
    css-loader 0.15.6 -> 0.16.0: https://github.com/webpack/css-loader/commits/master
    eslint 1.1.0 -> 1.2.1: https://github.com/eslint/eslint/blob/master/CHANGELOG.md
    eslint-plugin-react 3.2.2 -> 3.3.0: https://github.com/yannickcr/eslint-plugin-react/blob/master/History.md
    jest-cli 0.4.18 -> 0.5.0: https://github.com/facebook/jest/blob/master/CHANGELOG.md
    postcss 4.1.16 -> 0.5.0: https://github.com/postcss/postcss/blob/master/CHANGELOG.md
    postcss-nested 0.3.2 -> 1.0.0: https://github.com/postcss/postcss-nested/blob/master/CHANGELOG.md
    webpack 1.11.0 -> 1.12.0: https://github.com/webpack/webpack/commits/master
    webpack-hot-middleware 1.2.0 -> 2.0.0: https://github.com/glenjamin/webpack-hot-middleware#changelog

--- src/Router.js
+++ src/Router.js
@@ -31,7 +31,8 @@ const router = new Router(on => {
 
   on('error', (state, error) => state.statusCode === 404 ?
     <App context={state.context} error={error}><NotFoundPage /></App> :
-    <App context={state.context} error={error}><ErrorPage /></App>);
+    <App context={state.context} error={error}><ErrorPage /></App>
+  );
 
 });
 
