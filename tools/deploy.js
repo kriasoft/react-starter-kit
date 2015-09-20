b8bb6367b7b24c9e79ee893a5eccce3734b6641f
Konstantin Tarkus <hello@tarkus.me>
2015-09-20 09:16:11 +0300

    Update the deployment script
    ...with a basic Git-based deployment flow

--- tools/deploy.js
+++ tools/deploy.js
@@ -7,34 +7,48 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import push from 'git-push';
+import GitRepo from 'git-repository';
+import fetch from './lib/fetch';
+
+// TODO: Update deployment URL
+// For more information visit http://gitolite.com/deploy.html
+const getRemote = (slot) => ({
+  name: slot ? slot : 'production',
+  url: `https://example${slot ? '-' + slot : ''}.scm.azurewebsites.net:443/example.git`,
+  website: `http://example${slot ? '-' + slot : ''}.azurewebsites.net`,
+});
 
 /**
  * Deploy the contents of the `/build` folder to a remote
- * server via Git. Example: `npm run deploy -- staging`
+ * server via Git. Example: `npm run deploy -- production`
  */
-export default () => new Promise((resolve, reject) => {
-  let remote;
+export default async () => {
+  // By default deploy to the staging deployment slot
+  const remote = getRemote(process.argv.includes('production') ? null : 'staging');
 
-  if (process.argv.includes('production')) {
-    remote = {
-      name: 'production',
-      url: 'https://example.scm.azurewebsites.net/example.git',
-      branch: 'master',
-    };
-  } else if (process.argv.includes('staging')) {
-    remote = {
-      name: 'staging',
-      url: 'https://example-staging.scm.azurewebsites.net/example.git',
-      branch: 'master',
-    };
-  } else {
-    remote = {
-      name: 'test',
-      url: 'https://example-test.scm.azurewebsites.net/example.git',
-      branch: 'master',
-    };
+  // Initialize a new Git repository inside the `/build` folder
+  // if it doesn't exist yet
+  const repo = await GitRepo.open('build', { init: true });
+  await repo.setRemote(remote.name, remote.url);
+
+  // Fetch the remote repository if it exists
+  if ((await repo.hasRef(remote.url, 'master'))) {
+    await repo.fetch(remote.name);
+    await repo.reset(`${remote.name}/master`, { hard: true });
+    await repo.clean({ force: true });
   }
 
-  push('./build', remote, err => err ? reject(err) : resolve());
-});
+  // Build the project in RELEASE mode which
+  // generates optimized and minimized bundles
+  process.argv.push('release');
+  await require('./build')();
+
+  // Push the contents of the build folder to the remote server via Git
+  await repo.add('--all .');
+  await repo.commit('Update');
+  await repo.push(remote.name, 'master');
+
+  // Check if the site was successfully deployed
+  const response = await fetch(remote.website);
+  console.log(`${remote.website} -> ${response.statusCode}`);
+};
---
+++ tools/lib/fetch.js
@@ -0,0 +1,14 @@
+/**
+ * React Starter Kit (http://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2015 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import http from 'http';
+
+export default async (url) => new Promise((resolve, reject) =>
+    http.get(url, res => resolve(res)).on('error', err => reject(err))
+);
