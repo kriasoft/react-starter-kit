a6f4a9630fe2b3dc7df27054b06e4f6ff26fd6ec
materliu <materliu@gmail.com>
2015-12-09 16:48:52 +0800

    One really nice benefit of interpolated string literals is they are allowed to split across multiple lines

--- src/components/Html/Html.js
+++ src/components/Html/Html.js
@@ -20,12 +20,12 @@ class Html extends Component {
 
   trackingCode() {
     return ({ __html:
-      `(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=` +
-      `function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;` +
-      `e=o.createElement(i);r=o.getElementsByTagName(i)[0];` +
-      `e.src='https://www.google-analytics.com/analytics.js';` +
-      `r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));` +
-      `ga('create','${config.googleAnalyticsId}','auto');ga('send','pageview');`,
+      `(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+       function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+       e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+       e.src='https://www.google-analytics.com/analytics.js';
+       r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+       ga('create','${config.googleAnalyticsId}','auto');ga('send','pageview');`,
     });
   }
 
