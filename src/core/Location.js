860a403f2483f111f7748ba36aa088af82b5daa6
Konstantin Tarkus <hello@tarkus.me>
2016-02-20 15:50:00 +0300

    Improve browser detection in core/Location component

--- src/core/Location.js
+++ src/core/Location.js
@@ -7,11 +7,10 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
 import createHistory from 'history/lib/createBrowserHistory';
 import createMemoryHistory from 'history/lib/createMemoryHistory';
 import useQueries from 'history/lib/useQueries';
 
-const location = useQueries(canUseDOM ? createHistory : createMemoryHistory)();
+const location = useQueries(process.env.BROWSER ? createHistory : createMemoryHistory)();
 
 export default location;
