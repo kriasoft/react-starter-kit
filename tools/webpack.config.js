abf789500d5ffb5071d6c6c6515b9cc1b6e341d4
Vladimir Kutepov <frenzzy.man@gmail.com>
2016-03-08 23:23:02 +0300

    Discard comments from CSS in release mode

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -76,8 +76,17 @@ const config = {
         test: /\.scss$/,
         loaders: [
           'isomorphic-style-loader',
-          `css-loader?${DEBUG ? 'sourceMap&' : 'minimize&'}modules&localIdentName=` +
-          `${DEBUG ? '[name]_[local]_[hash:base64:3]' : '[hash:base64:4]'}`,
+          `css-loader?${JSON.stringify({
+            sourceMap: DEBUG,
+
+            // CSS Modules https://github.com/css-modules/css-modules
+            modules: true,
+            localIdentName: DEBUG ? '[name]_[local]_[hash:base64:3]' : '[hash:base64:4]',
+
+            // CSS Nano http://cssnano.co/options/
+            minimize: !DEBUG,
+            discardComments: { removeAll: true },
+          })}`,
           'postcss-loader?parser=postcss-scss',
         ],
       }, {
