335551c46db8c08cb81b4586f3cb14f9a3566789
whynot123 <aza3ejib@gmail.com>
2016-03-22 15:58:20 +0300

    add cleanPackage to build process

--- tools/build.js
+++ tools/build.js
@@ -11,6 +11,7 @@ import run from './run';
 import clean from './clean';
 import copy from './copy';
 import bundle from './bundle';
+import cleanPackage from './cleanPackage';
 
 /**
  * Compiles the project from source files into a distributable
@@ -19,6 +20,7 @@ import bundle from './bundle';
 async function build() {
   await run(clean);
   await run(copy);
+  await run(cleanPackage);
   await run(bundle);
 }
 
