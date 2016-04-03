ee187590d0d1d650ae2eff29f1ba2ea4e09b77e1
Ivan C. Sanches <sanches.ic.89@gmail.com>
2016-04-03 19:55:19 -0400

    Initial implementation of generate component and route

---
+++ tools/generate.js
@@ -0,0 +1,167 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import fs from '../lib/fs';
+
+const type = process.argv[3];
+const name = process.argv[4];
+const stateless = process.argb.includes('--stateless');
+
+function lowercaseFirstLetter(string) {
+  return string.charAt(0).toLowerCase() + string.slice(1);
+}
+
+const headerTemplate = [
+  '/**',
+  ' * React Starter Kit (https://www.reactstarterkit.com/)',
+  ' *',
+  ' * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.',
+  ' *',
+  ' * This source code is licensed under the MIT license found in the',
+  ' * LICENSE.txt file in the root directory of this source tree.',
+  ' */',
+  '',
+];
+
+const componentTemplate = [
+  ...headerTemplate,
+  "import React, { Component, PropTypes } from 'react';",
+  "import withStyles from 'isomorphic-style-loader/lib/withStyles';",
+  `import s from './${name}.scss';`,
+  '',
+  `class ${name} extends Component {`,
+  '',
+  '  static propTypes = {};',
+  '',
+  '  static contextTypes = {};',
+  '',
+  '  componentWillMount() {}',
+  '',
+  '  render() {',
+  '    return (<div className={s.root}></div>);',
+  '  }',
+  '',
+  '}',
+  '',
+  `export default withStyles(${name}, s);`,
+  '',
+];
+
+const componentStatelessTemplate = [
+  ...headerTemplate,
+  "import React from 'react';",
+  "import withStyles from 'isomorphic-style-loader/lib/withStyles';",
+  `import s from './${name}.scss';`,
+  '',
+  `function ${name}() {`,
+  '  return (<div className={s.root}></div>);',
+  '}',
+  '',
+  `export default withStyles(${name}, s);`,
+  '',
+];
+
+const componentStyleTemplate = [
+  ...headerTemplate,
+  "@import '../variables.scss';",
+  '',
+  '.root {}',
+  '',
+];
+
+const componentPackageTemplate = [
+  '{',
+  `  "name": "${name}",`,
+  '  "version": "0.0.0",',
+  '  "private": true,',
+  `  "main": "./${name}.js"`,
+  '}',
+];
+
+const routeName = lowercaseFirstLetter(name);
+
+const routeStyleTemplate = [
+  ...headerTemplate,
+  "@import '../../components/variables.scss';",
+  '',
+  '.root {}',
+  '',
+];
+
+const routeIndexTemplate = [
+  ...headerTemplate,
+  "import React from 'react';",
+  `import ${name} from './${name}';`,
+  '',
+  `export const path = '/${routeName}';`,
+  'export const action = async (state) => {',
+  `  const title = '${name}';`,
+  '  state.context.onSetTitle(title);',
+  `  return <${name} title={title} />;`,
+  '};',
+  '',
+];
+
+const templates = {
+  component: {
+    path: './src/components',
+    files: [
+      {
+        name: `${name}.js`,
+        template: {
+          default: componentTemplate,
+          stateless: componentStatelessTemplate,
+        },
+      },
+      {
+        name: `${name}.scss`,
+        template: componentStyleTemplate,
+      },
+      {
+        name: 'package.json',
+        template: componentPackageTemplate,
+      },
+    ],
+  },
+  route: {
+    path: './src/routes',
+    files: [
+      {
+        name: `${name}.js`,
+        template: {
+          default: componentTemplate,
+          stateless: componentStatelessTemplate,
+        },
+      },
+      {
+        name: `${name}.scss`,
+        template: routeStyleTemplate,
+      },
+      {
+        name: 'index.js',
+        template: routeIndexTemplate,
+      },
+    ],
+  },
+};
+
+async function generate() {
+  const template = templates[type];
+  const pathName = template.path + ((type === 'component') ? `/${name}` : `/${routeName}`);
+
+  await fs.makeDir(pathName);
+  template.files.forEach(async (file) => {
+    const content = (typeof file.template === 'string')
+      ? file.template
+      : file.template[(!stateless) ? 'default' : 'stateless'];
+    await fs.writeFile(`${pathName}/${file.name}`, content);
+  });
+}
+
+export default generate;
