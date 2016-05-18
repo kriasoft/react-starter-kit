8fcb0fa3aac032eb9e7d85e07a50703e54e0e8af
Konstantin Tarkus <koistya@gmail.com>
2016-05-18 18:39:46 +0300

    Add support for plain CSS (.css) files (#662)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@
 
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './Header.scss';
+import s from './Header.css';
 import Link from '../Link';
 import Navigation from '../Navigation';
 import logoUrl from './logo-small.png';
