d38150b64d5270e2c29d5426288966f92440f5d3
Harald Rudell <harald@attiresoft.com>
2016-07-14 11:25:21 -0700

    Allow callers of run to get resolution value

--- tools/run.js
+++ tools/run.js
@@ -17,12 +17,13 @@ function run(fn, options) {
   console.log(
     `[${format(start)}] Starting '${task.name}${options ? `(${options})` : ''}'...`
   );
-  return task(options).then(() => {
+  return task(options).then(resolution => {
     const end = new Date();
     const time = end.getTime() - start.getTime();
     console.log(
       `[${format(end)}] Finished '${task.name}${options ? `(${options})` : ''}' after ${time} ms`
     );
+    return resolution;
   });
 }
 
