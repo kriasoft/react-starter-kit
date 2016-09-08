84e86578aa7fab2f990a49cf48a84ad4e7617f7d
Vladimir Kutepov <frenzzy.man@gmail.com>
2016-09-09 00:10:09 +0300

    Clean up eslint rules and update airbnb config

--- src/client.js
+++ src/client.js
@@ -121,7 +121,7 @@ function run() {
       query: location.query,
       state: location.state,
       context,
-      render: render.bind(undefined, container, location), // eslint-disable-line react/jsx-no-bind, max-len
+      render: render.bind(undefined, container, location),
     }).catch(err => console.error(err)); // eslint-disable-line no-console
   }
 
--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -18,10 +18,11 @@ function isModifiedEvent(event) {
   return !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey);
 }
 
-class Link extends Component { // eslint-disable-line react/prefer-stateless-function
+class Link extends Component {
 
   static propTypes = {
     to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
+    children: PropTypes.node,
     onClick: PropTypes.func,
   };
 
@@ -55,8 +56,8 @@ class Link extends Component { // eslint-disable-line react/prefer-stateless-fun
   };
 
   render() {
-    const { to, ...props } = this.props; // eslint-disable-line no-use-before-define
-    return <a href={history.createHref(to)} {...props} onClick={this.handleClick} />;
+    const { to, children, ...props } = this.props;
+    return <a href={history.createHref(to)} {...props} onClick={this.handleClick}>{children}</a>;
   }
 
 }
