1b7507bb4578ce819fd40cee5199179679f09053
Kerem Sevencan <kerem@ritmix.org>
2016-10-04 18:19:49 +0300

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import user from './user';
 import runtime from './runtime';
 import intl from './intl';
 
 export default combineReducers({
+  user,
   runtime,
   intl,
 });
