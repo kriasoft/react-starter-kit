6a5b5d797487f3d696305771c9244d395a7369ef
CalcYu <Calcyu@qq.com>
2017-01-07 16:57:10 +0800

    'eslint'

--- src/routes/me/Me.js
+++ src/routes/me/Me.js
@@ -7,7 +7,8 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { PropTypes } from 'react';
+import React from 'react';
+import WEUI from 'react-weui';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Me.css';
 
@@ -21,7 +22,7 @@ import IconGeek5 from './assets/geek5_link.png';
 
 const smallIcon = <img className={s.iconRight} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII="/>;
 
-import WEUI from 'react-weui';
+
 const {
   Panel,
   PanelBody,
