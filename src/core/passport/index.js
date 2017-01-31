b3356f7f9ad0de408367819b73b298bc2df5967a
Clemens Sahs <clemens.sahs@slides-worker.org>
2017-02-01 00:25:55 +0100

    remove eslint env

--- src/core/passport/index.js
+++ src/core/passport/index.js
@@ -1,5 +1,4 @@
-/* eslint prefer-const: ["error", {"ignoreReadBeforeAssign": true}]*/
-/* eslint-env es6*//**
+/**
  * React Starter Kit (https://www.reactstarterkit.com/)
  *
  * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
