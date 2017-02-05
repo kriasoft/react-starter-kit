e490a5bdc1e48df2c522a1f950a12ca56a8fb862
Vladimir Kutepov <frenzzy.man@gmail.com>
2017-02-06 00:59:35 +0300

    Year-agnostic copyright message to prevent the need for yearly changes

--- tools/lib/fs.js
+++ tools/lib/fs.js
@@ -1,7 +1,7 @@
 /**
  * React Starter Kit (https://www.reactstarterkit.com/)
  *
- * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
