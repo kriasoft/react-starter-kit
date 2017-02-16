dcfbd43dd8cf00dbd16ed51ddae65c2466463a83
Pavel Lang <langpavel@phpskelet.org>
2017-02-16 04:45:06 +0100

    Use isDebug inside webpack.config instead of __DEV__

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -13,7 +13,7 @@ import AssetsPlugin from 'assets-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import pkg from '../package.json';
 
-const __DEV__ = !process.argv.includes('--release');
+const isDebug = !process.argv.includes('--release');
 const isVerbose = process.argv.includes('--verbose');
 const isAnalyse = process.argv.includes('--analyse') || process.argv.includes('--analyze');
 const port = parseInt(process.env.PORT || '3000', 10);
@@ -27,7 +27,7 @@ const analyzerPort = port + 3;
 let analyzerMode = 'disabled';
 if (isAnalyse) {
   analyzerMode = 'server';
-} else if (!__DEV__) {
+} else if (!isDebug) {
   analyzerMode = 'static';
 }
 
@@ -55,7 +55,7 @@ const config = {
         ],
         query: {
           // https://github.com/babel/babel-loader#options
-          cacheDirectory: __DEV__,
+          cacheDirectory: isDebug,
 
           // https://babeljs.io/docs/usage/options/
           babelrc: false,
@@ -78,15 +78,15 @@ const config = {
             'react',
             // Optimize React code for the production build
             // https://github.com/thejameskyle/babel-react-optimize
-            ...__DEV__ ? [] : ['react-optimize'],
+            ...isDebug ? [] : ['react-optimize'],
           ],
           plugins: [
             // Adds component stack to warning messages
             // https://github.com/babel/babel/tree/master/packages/babel-plugin-transform-react-jsx-source
-            ...__DEV__ ? ['transform-react-jsx-source'] : [],
+            ...isDebug ? ['transform-react-jsx-source'] : [],
             // Adds __self attribute to JSX which React will use for some warnings
             // https://github.com/babel/babel/tree/master/packages/babel-plugin-transform-react-jsx-self
-            ...__DEV__ ? ['transform-react-jsx-self'] : [],
+            ...isDebug ? ['transform-react-jsx-self'] : [],
           ],
         },
       },
@@ -101,12 +101,12 @@ const config = {
             options: {
               // CSS Loader https://github.com/webpack/css-loader
               importLoaders: 1,
-              sourceMap: __DEV__,
+              sourceMap: isDebug,
               // CSS Modules https://github.com/css-modules/css-modules
               modules: true,
-              localIdentName: __DEV__ ? '[name]-[local]-[hash:base64:5]' : '[hash:base64:5]',
+              localIdentName: isDebug ? '[name]-[local]-[hash:base64:5]' : '[hash:base64:5]',
               // CSS Nano http://cssnano.co/options/
-              minimize: !__DEV__,
+              minimize: !isDebug,
               discardComments: { removeAll: true },
             },
           },
@@ -130,14 +130,14 @@ const config = {
         test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
         loader: 'file-loader',
         query: {
-          name: __DEV__ ? '[path][name].[ext]?[hash:8]' : '[hash:8].[ext]',
+          name: isDebug ? '[path][name].[ext]?[hash:8]' : '[hash:8].[ext]',
         },
       },
       {
         test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
         loader: 'url-loader',
         query: {
-          name: __DEV__ ? '[path][name].[ext]?[hash:8]' : '[hash:8].[ext]',
+          name: isDebug ? '[path][name].[ext]?[hash:8]' : '[hash:8].[ext]',
           limit: 10000,
         },
       },
@@ -149,13 +149,13 @@ const config = {
   },
 
   // Don't attempt to continue if there are any errors.
-  bail: !__DEV__,
+  bail: !isDebug,
 
-  cache: __DEV__,
+  cache: isDebug,
 
   stats: {
     colors: true,
-    reasons: __DEV__,
+    reasons: isDebug,
     hash: isVerbose,
     version: isVerbose,
     timings: true,
@@ -182,8 +182,8 @@ const clientConfig = {
 
   output: {
     ...config.output,
-    filename: __DEV__ ? '[name].js' : '[name].[chunkhash:8].js',
-    chunkFilename: __DEV__ ? '[name].chunk.js' : '[name].[chunkhash:8].chunk.js',
+    filename: isDebug ? '[name].js' : '[name].[chunkhash:8].js',
+    chunkFilename: isDebug ? '[name].chunk.js' : '[name].[chunkhash:8].chunk.js',
   },
 
   resolve: { ...config.resolve },
@@ -192,9 +192,9 @@ const clientConfig = {
     // Define free variables
     // https://webpack.github.io/docs/list-of-plugins.html#defineplugin
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': __DEV__ ? '"development"' : '"production"',
+      'process.env.NODE_ENV': isDebug ? '"development"' : '"production"',
       'process.env.BROWSER': true,
-      __DEV__,
+      __DEV__: isDebug,
     }),
 
     // Emit a file with assets paths
@@ -212,7 +212,7 @@ const clientConfig = {
       minChunks: module => /node_modules/.test(module.resource),
     }),
 
-    ...__DEV__ ? [] : [
+    ...isDebug ? [] : [
       // Minimize all JavaScript output of chunks
       // https://github.com/mishoo/UglifyJS2#compressor-options
       new webpack.optimize.UglifyJsPlugin({
@@ -246,7 +246,7 @@ const clientConfig = {
       // Automatically open report in default browser
       openAnalyzer: true,
       // If `true`, Webpack Stats JSON file will be generated in bundles output directory
-      generateStatsFile: !__DEV__,
+      generateStatsFile: !isDebug,
       // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
       // Relative to bundles output directory.
       statsFilename: path.resolve(__dirname, '../stats.json'),
@@ -261,7 +261,7 @@ const clientConfig = {
 
   // Choose a developer tool to enhance debugging
   // http://webpack.github.io/docs/configuration.html#devtool
-  devtool: __DEV__ ? 'cheap-module-source-map' : false,
+  devtool: isDebug ? 'cheap-module-source-map' : false,
 
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
@@ -330,9 +330,9 @@ const serverConfig = {
     // Define free variables
     // https://webpack.github.io/docs/list-of-plugins.html#defineplugin
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': __DEV__ ? '"development"' : '"production"',
+      'process.env.NODE_ENV': isDebug ? '"development"' : '"production"',
       'process.env.BROWSER': false,
-      __DEV__,
+      __DEV__: isDebug,
     }),
 
     // Do not create separate chunks of the server bundle
@@ -357,7 +357,7 @@ const serverConfig = {
     __dirname: false,
   },
 
-  devtool: __DEV__ ? 'cheap-module-source-map' : 'source-map',
+  devtool: isDebug ? 'cheap-module-source-map' : 'source-map',
 };
 
 export default [clientConfig, serverConfig];
