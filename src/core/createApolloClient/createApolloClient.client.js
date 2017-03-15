85b3367b2268a8ae1101aa10bd3c6ad33ea35e03
Pavel Lang <langpavel@phpskelet.org>
2017-03-16 00:30:13 +0100

    Include credentials with Apollo request

--- src/core/createApolloClient/createApolloClient.client.js
+++ src/core/createApolloClient/createApolloClient.client.js
@@ -3,6 +3,9 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client';
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
     uri: '/graphql',
+    opts: {
+      credentials: 'include',
+    },
   }),
   queryDeduplication: true,
   reduxRootSelector: state => state.apollo,
