6c1b54cee54d4f2f03bc2347ea7d24e868ccbcde
Konstantin Tarkus <hello@tarkus.me>
2017-04-25 11:58:06 +0300

    Make createFetch more readable

--- src/createFetch.js
+++ src/createFetch.js
@@ -7,28 +7,43 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import isoFetch from 'isomorphic-fetch';
+/* @flow */
 
-type FetchOptions = {
+import fetch from 'isomorphic-fetch';
+
+type Options = {
   baseUrl: string,
   cookie?: string,
 };
 
-function createFetch({ baseUrl, cookie }: FetchOptions) {
-  return function fetch(url, options) {
-    const { headers, ...other } = options || {};
-    return url.startsWith('/') ? isoFetch(`${baseUrl}${url}`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        ...(typeof cookie === 'undefined' ? null : { cookie }),
-        ...headers,
-      },
-      credentials: 'include',
-      ...other,
-    }) : isoFetch(url, options);
+/**
+ * Creates a wrapper function around the HTML5 Fetch API that provides
+ * default arguments to fetch(...) and is intended to reduce the amount
+ * of boilerplate code in the application.
+ * https://developer.mozilla.org/docs/Web/API/Fetch_API/Using_Fetch
+ */
+function createFetch({ baseUrl, cookie }: Options) {
+  // NOTE: Tweak the default options to suite your application needs
+  const defaults = {
+    method: 'POST', // handy with GraphQL backends
+    mode: baseUrl ? 'cors' : 'same-origin',
+    credentials: baseUrl ? 'include' : 'same-origin',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      ...(cookie ? { Cookie: cookie } : null),
+    },
   };
+
+  return (url, options) => ((url.startsWith('/graphql') || url.startsWith('/api')) ?
+    fetch(`${baseUrl}${url}`, {
+      ...defaults,
+      ...options,
+      headers: {
+        ...defaults.headers,
+        ...(options && options.headers),
+      },
+    }) : fetch(url, options));
 }
 
 export default createFetch;
