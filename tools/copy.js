499f0868d8d2970da5b04356e1ac7a74966cf0d0
Vladimir Kutepov <frenzzy.man@gmail.com>
2017-04-29 16:41:42 +0300

    Move stylelint config to .stylelintrc.js; add comments

--- tools/copy.js
+++ tools/copy.js
@@ -56,7 +56,7 @@ async function copy() {
       }
       const end = new Date();
       const time = end.getTime() - start.getTime();
-      console.log(`[${format(end)}] ${event} '${dist}' after ${time} ms`);
+      console.info(`[${format(end)}] ${event} '${dist}' after ${time} ms`);
     });
   }
 }
