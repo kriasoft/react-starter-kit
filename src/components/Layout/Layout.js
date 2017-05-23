81a256db311bb8defb985c26434e6404b1c259e6
Pavel Lang <langpavel@phpskelet.org>
2017-05-23 22:14:05 +0200

    fix: Import external global CSS from JS

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -10,6 +10,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+// external-global styles must be imported in your JS.
+import normalizeCss from 'normalize.css';
 import s from './Layout.css';
 import Header from '../Header';
 import Feedback from '../Feedback';
@@ -32,4 +35,4 @@ class Layout extends React.Component {
   }
 }
 
-export default withStyles(s)(Layout);
+export default withStyles(normalizeCss, s)(Layout);
