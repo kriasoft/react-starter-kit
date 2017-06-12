45cc27908e7733ad5479dd78d703ed51865ee686
Vladimir Kutepov <frenzzy.man@gmail.com>
2017-06-12 14:44:32 +0300

    Add stylelint-order; remove stylefmt in favor of stylelint --fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,56 +10,56 @@
 // ESLint configuration
 // http://eslint.org/docs/user-guide/configuring
 module.exports = {
-    parser: 'babel-eslint',
+  parser: 'babel-eslint',
 
-    extends: [
-      'airbnb',
-      'plugin:css-modules/recommended',
+  extends: [
+    'airbnb',
+    'plugin:css-modules/recommended',
+  ],
+
+  plugins: [
+    'css-modules',
+  ],
+
+  globals: {
+    __DEV__: true,
+  },
+
+  env: {
+    browser: true,
+  },
+
+  rules: {
+    // `js` and `jsx` are common extensions
+    // `mjs` is for `universal-router` only, for now
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        jsx: 'never',
+        mjs: 'never',
+      },
     ],
 
-    plugins: [
-      'css-modules',
+    // Not supporting nested package.json yet
+    // https://github.com/benmosher/eslint-plugin-import/issues/458
+    'import/no-extraneous-dependencies': 'off',
+
+    // Recommend not to leave any console.log in your code
+    // Use console.error, console.warn and console.info instead
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error', 'info'],
+      },
     ],
 
-    globals: {
-      __DEV__: true,
-    },
+    // Allow js files to use jsx syntax, too
+    'react/jsx-filename-extension': 'off',
 
-    env: {
-      browser: true,
-    },
-
-    rules: {
-      // `js` and `jsx` are common extensions
-      // `mjs` is for `universal-router` only, for now
-      'import/extensions': [
-        'error',
-        'always',
-        {
-          js: 'never',
-          jsx: 'never',
-          mjs: 'never',
-        },
-      ],
-
-      // Not supporting nested package.json yet
-      // https://github.com/benmosher/eslint-plugin-import/issues/458
-      'import/no-extraneous-dependencies': 'off',
-
-      // Recommend not to leave any console.log in your code
-      // Use console.error, console.warn and console.info instead
-      'no-console': [
-        'error',
-        {
-          allow: ['warn', 'error', 'info'],
-        },
-      ],
-
-      // Allow js files to use jsx syntax, too
-      'react/jsx-filename-extension': 'off',
-
-      // https://github.com/kriasoft/react-starter-kit/pull/961
-      // You can reopen this if you still want this rule
-      'react/prefer-stateless-function': 'off',
-    },
-  };
+    // https://github.com/kriasoft/react-starter-kit/pull/961
+    // You can reopen this if you still want this rule
+    'react/prefer-stateless-function': 'off',
+  },
+};
