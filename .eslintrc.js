7d0a471e8ae5cfdfee121659ec505fd149afbd65
Vladimir Kutepov <frenzzy.man@gmail.com>
2017-06-14 21:09:03 +0300

    Support for absolute imports (#1312)

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,4 +62,14 @@ module.exports = {
     // You can reopen this if you still want this rule
     'react/prefer-stateless-function': 'off',
   },
+
+  settings: {
+    // Allow absolute paths in imports, e.g. import Button from 'components/Button'
+    // https://github.com/benmosher/eslint-plugin-import/tree/master/resolvers
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+  },
 };
