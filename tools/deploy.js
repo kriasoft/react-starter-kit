ffd67aeb68061d0e4ccb3b5e4e37a330ac5d1cef
sreym <sergeyreym@gmail.com>
2017-08-05 19:02:41 +0800

    add config for deploying to Heroku

--- tools/deploy.js
+++ tools/deploy.js
@@ -14,21 +14,21 @@ import { makeDir, moveDir, cleanDir } from './lib/fs';
 import run from './run';
 
 // GitHub Pages
-const remote = {
-  name: 'github',
-  url: 'https://github.com/<user>/<repo>.git',
-  branch: 'gh-pages',
-  website: 'https://<user>.github.io/<repo>/',
-  static: true,
-};
+// const remote = {
+//   name: 'github',
+//   url: 'https://github.com/<user>/<repo>.git',
+//   branch: 'gh-pages',
+//   website: 'https://<user>.github.io/<repo>/',
+//   static: true,
+// };
 
 // Heroku
-// const remote = {
-//   name: 'heroku',
-//   url: 'https://git.heroku.com/<app>.git',
-//   branch: 'master',
-//   website: 'https://<app>.herokuapp.com',
-// };
+const remote = {
+  name: 'heroku',
+  url: 'https://git.heroku.com/ndo.git',
+  branch: 'master',
+  website: 'https://ndo.herokuapp.com',
+};
 
 // Azure Web Apps
 // const remote = {
