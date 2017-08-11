c983f21951a011dda99cac82e50dbe18e0301749
ithebredor <fusrodah112311@gmail.com>
2017-08-11 12:41:47 +0800

    Moved to react-bootstrap

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,6 +8,7 @@
  */
 
 import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Navigation.css';
 import Link from '../Link';
@@ -15,60 +16,38 @@ import Link from '../Link';
 class Navigation extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#navbar"
-              aria-expanded="false"
-              aria-controls="navbar"
-            >
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-            </button>
-            <a className="navbar-brand" href="/">
-              NDO
-            </a>
-          </div>
-          <div id="navbar" className="navbar-collapse collapse">
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <Link to="/courses">Курсы</Link>
-              </li>
-              <li>
-                <Link to="/users">Пользователи</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-              <li>
-                <Link to="/login">Log in</Link>
-              </li>
-              <li>
-                <span className="navbar-text">or</span>
-              </li>
-              <li>
-                <Link to="/register">Sign up</Link>
-              </li>
-            </ul>
-            <form className="navbar-form navbar-right">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Search..."
-              />
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Navbar inverse collapseOnSelect fixedTop defaultExpanded fluid>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">NDO</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <li>
+              <Link to="/courses">Курсы</Link>
+            </li>
+            <li>
+              <Link to="/users">Пользователи</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </Nav>
+          <Nav pullRight>
+            <li>
+              <Link to="/login">Log in</Link>
+            </li>
+            <li>
+              <Link to="/register">Sign up</Link>
+            </li>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
