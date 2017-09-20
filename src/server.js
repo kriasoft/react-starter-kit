a9f37d7bb913984346aff8f641efbc6472b59e03
Vladimir Kutepov <frenzzy.man@gmail.com>
2017-09-20 21:23:32 +0300

    Update UniversalRouter to v4 (#1403)

--- src/server.js
+++ src/server.js
@@ -132,7 +132,7 @@ app.get('*', async (req, res, next) => {
 
     const route = await router.resolve({
       ...context,
-      path: req.path,
+      pathname: req.path,
       query: req.query,
     });
 
