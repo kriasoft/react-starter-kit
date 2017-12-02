e1783cac76e477def3f869b32a79e370f3bcb27d
sreym <sergeyreym@gmail.com>
2017-12-02 16:44:40 +0800

    fix title

--- src/routes/index.js
+++ src/routes/index.js
@@ -76,7 +76,7 @@ const routes = {
     const route = await next();
 
     // Provide default values for title, description etc.
-    route.title = `${route.title || 'Untitled Page'} - www.reactstarterkit.com`;
+    route.title = `${route.title || 'Untitled Page'} - NDO`;
     route.description = route.description || '';
 
     return route;
