965e9003fb9445516a2776a873400391dc86570b
ilyaabramovich <ilyaabramovich1998@gmail.com>
2017-12-09 13:43:09 +0800

    'subscribed users' is loaded properly

--- src/routes/course/Course.js
+++ src/routes/course/Course.js
@@ -33,6 +33,11 @@ class Course extends React.Component {
           title: PropTypes.string,
         }),
       ),
+      users: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string,
+        }),
+      ),
     }).isRequired,
   };
 
@@ -58,6 +63,7 @@ class Course extends React.Component {
   componentWillMount() {
     this.setState({
       studyEntities: this.context.store.getState().course.studyEntities,
+      subscribedUsersList: this.props.course.users.map(u => u.id),
     });
   }
 
