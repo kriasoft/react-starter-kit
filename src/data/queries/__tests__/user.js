80210b7d9b8d62925f0dd6396ee37956f3315197
sreym <sergeyreym@gmail.com>
2017-12-16 13:34:54 +0800

    added sample test for graphql (user.js)

---
+++ src/data/queries/__tests__/user.js
@@ -0,0 +1,37 @@
+/* eslint-env jest */
+/* eslint-disable padded-blocks, no-unused-expressions */
+
+import 'babel-regenerator-runtime';
+import { graphql } from 'graphql';
+import config from '../../../config';
+import schema from '../../schema';
+import models from '../../models';
+
+async function setupTest() {
+  config.databaseUrl = 'sqlite:database_test.sqlite';
+  await models.sync({ force: true });
+}
+
+beforeEach(async () => setupTest());
+
+describe('graphql user', () => {
+  test('create', async () => {
+    const Q = `mutation create($email:String, $key:String, $name:String, $gender:String) {
+      createUser(email: $email, key: $key, name: $name, gender: $gender) {
+        id, email, profile { gender }
+      }
+    }`;
+    const email = 'test@test.com';
+    const name = 'Test User';
+    const gender = 'male';
+    const res = await graphql(schema, Q, null, null, {
+      email,
+      key: '1234',
+      name,
+      gender,
+    });
+    expect(res.data.createUser.id).toBeDefined();
+    expect(res.data.createUser.email).toBe(email);
+    expect(res.data.createUser.profile.gender).toBe(gender);
+  });
+});
