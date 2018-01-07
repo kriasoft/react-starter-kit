4d351f46f4689e14c0bda08c719040e0a6e6e67a
Pavel Lang <langpavel@phpskelet.org>
2018-01-07 13:41:30 +0100

    typos

--- src/core/createApolloClient/createCache.js
+++ src/core/createApolloClient/createCache.js
@@ -5,11 +5,11 @@ import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory';
 function dataIdFromObject(obj) {
   switch (obj.__typename) {
     case 'IntlMessage':
-      // Do not use id as unique identifier for message because it's not between languages
+      // Do not use id as identifier for message because it's not unique between languages
       // instead instruct cache to build path and index identifier for cache key
       return null;
     case 'NewsItem':
-      return obj.link ? `NewsItem.${obj.link}` : defaultDataIdFromObject(obj);
+      return obj.link ? `NewsItem:${obj.link}` : defaultDataIdFromObject(obj);
     default:
       return defaultDataIdFromObject(obj);
   }
