fb2a4df784c826ce3b66b562dc6bf3e9c879cac5
Rodey <me@rodey.nl>
2018-02-05 13:56:44 +0100

    Updated @decorators & babel

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -104,7 +104,7 @@ const config = {
             ['@babel/preset-react', { development: isDebug }],
           ],
           plugins: [
-            'transform-decorators-legacy',
+            '@babel/plugin-proposal-decorators',
             // Treat React JSX elements as value types and hoist them to the highest scope
             // https://github.com/babel/babel/tree/master/packages/babel-plugin-transform-react-constant-elements
             ...(isDebug ? [] : ['@babel/transform-react-constant-elements']),
