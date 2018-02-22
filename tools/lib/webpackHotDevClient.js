1f5fb645c01d91e3668fd3e3603b991532f38900
Jeroen Wever <jeroenrwever@gmail.com>
2018-02-22 13:30:49 +0100

    Comment clean up + removal of CONTRIBUTING.md

--- tools/lib/webpackHotDevClient.js
+++ tools/lib/webpackHotDevClient.js
@@ -1,12 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import hotClient from 'webpack-hot-middleware/client';
 import launchEditorEndpoint from 'react-dev-utils/launchEditorEndpoint';
 import formatWebpackMessages from 'react-dev-utils/formatWebpackMessages';
