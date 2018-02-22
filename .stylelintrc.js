463feca4d53dd85d34577fa56b2ae69e445bb50a
Rodey <me@rodey.nl>
2018-02-22 11:22:18 +0100

    Compose now is always on the top and then the alphabetical order of the css

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 // stylelint configuration
 // https://stylelint.io/user-guide/configuration/
 module.exports = {
@@ -57,8 +48,15 @@ module.exports = {
     ],
 
     // https://github.com/hudochenkov/stylelint-order/blob/master/rules/properties-order/README.md
-    'order/properties-order': [],
-
-    'order/properties-alphabetical-order': true,
+    'order/properties-order': [
+      [
+        {
+          properties: ['composes'],
+        },
+      ],
+      {
+        unspecified: 'bottomAlphabetical',
+      },
+    ],
   },
 };
