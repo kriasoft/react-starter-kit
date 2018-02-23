7242d9207e1f17e34b914516845113acd58524a2
Ben Kwok <ben.kwok.cy@gmail.com>
2018-02-23 14:14:47 -0800

    Remove feedback component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,7 +17,7 @@ class Footer extends React.Component {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <span className={s.text}>© Your Company</span>
+          <span className={s.text}>© Serious Groceries</span>
           <span className={s.spacer}>·</span>
           <Link className={s.link} to="/">
             Home
