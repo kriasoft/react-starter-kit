e9a5216aa2261749870a95991f17eab06f050779
Ben Kwok <ben.kwok.cy@gmail.com>
2018-02-23 15:55:10 -0800

    Change double quotes to single quotes

--- src/routes/today/index.js
+++ src/routes/today/index.js
@@ -11,7 +11,7 @@ import React from 'react';
 import Layout from '../../components/Layout';
 import Today from './Today';
 
-const title = "Your Options";
+const title = 'Your Options';
 
 function action() {
   return {
