30feb49c13eee1a9fa2cdbf34de14bd1f783198a
Ben Kwok <ben.kwok.cy@gmail.com>
2018-02-23 17:20:43 -0800

    Add Privacy Policy Text and Remove Not Found Link from Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -30,10 +30,6 @@ class Footer extends React.Component {
           <Link className={s.link} to="/privacy">
             Privacy
           </Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/not-found">
-            Not Found
-          </Link>
         </div>
       </div>
     );
