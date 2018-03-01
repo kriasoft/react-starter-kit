aa11249ddddac165444de262455c651178d2b2e1
Ben Kwok <ben.kwok.cy@gmail.com>
2018-02-28 16:14:40 -0800

    Set up ReactTable

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -9,20 +9,48 @@
 
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import { JsonTable } from 'react-json-table';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 import s from './Table.css';
 
 class Table extends React.Component {
   render() {
-    const items = [
-      { name: 'Louise', age: 27, color: 'red' },
-      { name: 'Margaret', age: 15, color: 'blue' },
-      { name: 'Lisa', age: 34, color: 'yellow' },
+    const data = [
+      {
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        },
+      },
     ];
+
+    const columns = [
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Age',
+        accessor: 'age',
+        Cell: props => <span className="number">{props.value}</span>, // Custom cell components!
+      },
+      {
+        id: 'friendName', // Required because our accessor is not a string
+        Header: 'Friend Name',
+        accessor: d => d.friend.name, // Custom value accessors!
+      },
+      // {
+      //   Header: props => <span>Friend Age</span>, // Custom header components!
+      //   accessor: 'friend.age',
+      // },
+    ];
+
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <JsonTable rows={items} />
+          <ReactTable data={data} columns={columns} />
         </div>
       </div>
     );
