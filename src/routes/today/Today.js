2f5824b08456fd0e6c7c0a47cf5556e8f632934f
Ben Kwok <ben.kwok.cy@gmail.com>
2018-02-28 16:46:01 -0800

    Added React-Table CSS

--- src/routes/today/Today.js
+++ src/routes/today/Today.js
@@ -11,7 +11,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import ReactTable from 'react-table';
-import 'react-table/react-table.css';
+import t from 'react-table/react-table.css';
 import s from './Today.css';
 
 class Today extends React.Component {
@@ -24,10 +24,12 @@ class Today extends React.Component {
       {
         name: 'Tanner Linsley',
         age: 26,
-        friend: {
-          name: 'Jason Maurer',
-          age: 23,
-        },
+        job: 'Ironsmith',
+      },
+      {
+        name: 'Jacob Holier',
+        age: 31,
+        job: 'Linter',
       },
     ];
 
@@ -39,47 +41,24 @@ class Today extends React.Component {
       {
         Header: 'Age',
         accessor: 'age',
-        Cell: props => <span className="number">{props.value}</span>, // Custom cell components!
+        // Cell: props => <span className="number">{props.value}</span>, // Custom cell components!
       },
       {
-        id: 'friendName', // Required because our accessor is not a string
-        Header: 'Friend Name',
-        accessor: d => d.friend.name, // Custom value accessors!
+        Header: 'Job',
+        accessor: 'job',
       },
-      // {
-      //   Header: props => <span>Friend Age</span>, // Custom header components!
-      //   accessor: 'friend.age',
-      // },
     ];
     return (
       <div className={s.root}>
         <div className={s.container}>
           <h1>{this.props.title}</h1>
-          <ReactTable data={data} columns={columns} />
-          {/* <table className={s.table}>
-            <tr>
-              <td>
-                <img
-                  className={s.pic}
-                  src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg"
-                  alt=""
-                />
-              </td>
-            </tr>
-            <tr>
-              <th>Margherita Pizza</th>
-            </tr>
-            <tr>
-              <td>Picture Here</td>
-            </tr>
-            <tr>
-              <td>Price</td>
-            </tr>
-          </table> */}
+          <div className={t}>
+            <ReactTable data={data} columns={columns} />
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default withStyles(s)(Today);
+export default withStyles(s, t)(Today);
