86a57f33b4ffac97ebd38457d875d5daa75485b0
Jorrit Schippers <jorrit@ncode.nl>
2018-03-25 01:29:51 +0100

    fix(webpack): Fix Webpack compilation on Windows (#1578)

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -17,7 +17,7 @@ import overrideRules from './lib/overrideRules';
 import pkg from '../package.json';
 
 const ROOT_DIR = path.resolve(__dirname, '..');
-const SRC_DIR = `${ROOT_DIR}/src`;
+const SRC_DIR = path.join(ROOT_DIR, 'src');
 const BUILD_DIR = `${ROOT_DIR}/build`;
 const NODE_MODULES_DIR = `${ROOT_DIR}/node_modules`;
 
