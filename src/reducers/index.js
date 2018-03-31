95c3bc41ec8fb05dd6b70bb4cb9047ad0d8818f7
ilyaabramovich <ilyaabramovich1998@gmail.com>
2018-03-31 13:56:03 +0900

    studyEntity -> study-entity

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import user from './user';
 import runtime from './runtime';
 import courses from './courses';
 import course from './course';
-import studyEntity from './studyEntity';
+import studyEntity from './study-entity';
 import users from './users';
 import files from './files';
 
