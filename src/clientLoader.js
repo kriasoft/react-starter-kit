fc3bc2915375cc16cd7c8dee67dccf51230b5886
reisel <dreisel@gmail.com>
2018-06-19 05:10:01 +0300

    bug(polyfill) - fix polyfill loading. (#1583)

--- src/clientLoader.js
+++ src/clientLoader.js
@@ -1,15 +1,16 @@
-// polyfills
-import main from './client';
+const readyStates = new Set(['complete', 'loaded', 'interactive']);
+
+function loadMainClient() {
+  const main = require('./client').default; // eslint-disable-line global-require
+  main();
+}
 
 function run() {
   // Run the application when both DOM is ready and page content is loaded
-  if (
-    ['complete', 'loaded', 'interactive'].includes(document.readyState) &&
-    document.body
-  ) {
-    main();
+  if (readyStates.has(document.readyState) && document.body) {
+    loadMainClient();
   } else {
-    document.addEventListener('DOMContentLoaded', main, false);
+    document.addEventListener('DOMContentLoaded', loadMainClient, false);
   }
 }
 
