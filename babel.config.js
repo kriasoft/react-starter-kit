5830732641494ac5d4df6f39f44ebdfaf88e7cf0
mapkeji <mapkeji26@gmail.com>
2018-07-30 23:22:09 +0300

    Update dependencies and change babel

--- babel.config.js
+++ babel.config.js
@@ -19,9 +19,12 @@ module.exports = {
         },
       },
     ],
-    ['@babel/preset-stage-2', { decoratorsLegacy: true }],
     '@babel/preset-flow',
     '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
   ],
   ignore: ['node_modules', 'build'],
 };
--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -98,9 +98,6 @@ const config = {
                 debug: false,
               },
             ],
-            // Experimental ECMAScript proposals
-            // https://babeljs.io/docs/plugins/#presets-stage-x-experimental-presets-
-            ['@babel/preset-stage-2', { decoratorsLegacy: true }],
             // Flow
             // https://github.com/babel/babel/tree/master/packages/babel-preset-flow
             '@babel/preset-flow',
@@ -109,6 +106,8 @@ const config = {
             ['@babel/preset-react', { development: isDebug }],
           ],
           plugins: [
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-syntax-dynamic-import',
             // Treat React JSX elements as value types and hoist them to the highest scope
             // https://github.com/babel/babel/tree/master/packages/babel-plugin-transform-react-constant-elements
             ...(isDebug ? [] : ['@babel/transform-react-constant-elements']),
