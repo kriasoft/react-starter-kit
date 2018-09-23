a1fbbb5e741a725ddeaf8dfeaf59dfeedca0d570
sreym <sergeyreym@gmail.com>
2018-09-23 21:55:25 +0300

    refactoring

--- src/data/models/Answer.js
+++ src/data/models/Answer.js
@@ -14,9 +14,9 @@ const Answer = Model.define('Answer', {
   },
 });
 
-class SchemaChecker {
+class AnswerChecker {
   static create(schema) {
-    const checker = new SchemaChecker();
+    const checker = new AnswerChecker();
     checker.schema =
       typeof schema === 'string'
         ? (checker.schema = JSON.parse(schema))
@@ -24,17 +24,19 @@ class SchemaChecker {
     checker.names = Object.keys(checker.schema).filter(
       k => checker.schema[k].checker,
     );
-    checker.fns = checker.names.reduce((fns, k) => {
-      fns[k] = checker.build(k);
-      return fns;
-    }, {});
+    checker.fns = {};
+    checker.names.forEach(k => checker.build(k));
     return checker;
   }
 
   // TODO: use nodejs vm instead of eval
+  /**
+   * Any preprocessing related to checker function for key
+   * @param {string} key fn key
+   */
   build(key) {
     /* eslint-disable no-new-func */
-    return new Function(
+    this.fns[key] = new Function(
       'val',
       'key',
       'doc',
@@ -43,15 +45,28 @@ class SchemaChecker {
     );
   }
 
+  /**
+   * Runs checker for the specified key
+   * @returns {number} mark
+   * @param {object} answer parsed answer object
+   * @param {*} key key in answer object
+   */
   run(answer, key) {
     return this.fns[key](answer[key], key, answer, this.schema);
   }
 }
 
+/**
+ * Parses schema & answer, runs checker functions if exist and combine results
+ * as an object with mark & comment
+ * @returns {{mark: number, comment: string}}
+ * @param {string} answerStr answer in JSON
+ * @param {string|object} schema represents unit schema
+ */
 function getMarkForAnswer(answerStr, schema) {
   if (!answerStr) return {};
   const answer = JSON.parse(answerStr);
-  const checker = SchemaChecker.create(schema);
+  const checker = AnswerChecker.create(schema);
   const res = { mark: 0, comment: 'Marks:' };
   for (let i = 0; i < checker.names.length; i += 1) {
     const m = checker.run(answer, checker.names[i]);
