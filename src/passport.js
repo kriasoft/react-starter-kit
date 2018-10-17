12d002f3b2f259f2090be48abe6a0c99ae2ec5dc
sreym <sergeyreym@gmail.com>
2018-10-17 10:49:09 +0800

    fix code duplicate in passport.js

--- src/passport.js
+++ src/passport.js
@@ -40,7 +40,32 @@ passport.use(
       passReqToCallback: true,
     },
     (req, accessToken, refreshToken, profile, done) => {
-      /* eslint-disable no-underscore-dangle */
+      const createFacebookUser = async params => {
+        /* eslint-disable no-underscore-dangle */
+        const user = await User.create(
+          {
+            ...params,
+            profile: {
+              displayName: profile.displayName,
+              gender: profile._json.gender,
+              picture: `https://graph.facebook.com/${
+                profile.id
+              }/picture?type=large`,
+            },
+          },
+          {
+            include: [
+              { model: UserLogin, as: 'logins' },
+              { model: UserClaim, as: 'claims' },
+              { model: UserProfile, as: 'profile' },
+            ],
+          },
+        );
+        done(null, {
+          id: user.id,
+          email: user.email,
+        });
+      };
       const loginName = 'facebook';
       const claimType = 'urn:facebook:access_token';
       const fooBar = async () => {
@@ -54,31 +79,11 @@ passport.use(
             // Sign in with that account or delete it, then link it with your current account.
             done();
           } else {
-            const user = await User.create(
-              {
-                id: req.user.id,
-                email: profile._json.email,
-                logins: [{ name: loginName, key: profile.id }],
-                claims: [{ type: claimType, value: profile.id }],
-                profile: {
-                  displayName: profile.displayName,
-                  gender: profile._json.gender,
-                  picture: `https://graph.facebook.com/${
-                    profile.id
-                  }/picture?type=large`,
-                },
-              },
-              {
-                include: [
-                  { model: UserLogin, as: 'logins' },
-                  { model: UserClaim, as: 'claims' },
-                  { model: UserProfile, as: 'profile' },
-                ],
-              },
-            );
-            done(null, {
-              id: user.id,
-              email: user.email,
+            createFacebookUser({
+              id: req.user.id,
+              email: profile._json.email,
+              logins: [{ name: loginName, key: profile.id }],
+              claims: [{ type: claimType, value: profile.id }],
             });
           }
         } else {
@@ -98,7 +103,7 @@ passport.use(
             const user = users[0].get({ plain: true });
             done(null, user);
           } else {
-            let user = await User.findOne({
+            const user = await User.findOne({
               where: { email: profile._json.email },
             });
             if (user) {
@@ -106,31 +111,11 @@ passport.use(
               // that account and link it with Facebook manually from Account Settings.
               done(null);
             } else {
-              user = await User.create(
-                {
-                  email: profile._json.email,
-                  emailConfirmed: true,
-                  logins: [{ name: loginName, key: profile.id }],
-                  claims: [{ type: claimType, value: accessToken }],
-                  profile: {
-                    displayName: profile.displayName,
-                    gender: profile._json.gender,
-                    picture: `https://graph.facebook.com/${
-                      profile.id
-                    }/picture?type=large`,
-                  },
-                },
-                {
-                  include: [
-                    { model: UserLogin, as: 'logins' },
-                    { model: UserClaim, as: 'claims' },
-                    { model: UserProfile, as: 'profile' },
-                  ],
-                },
-              );
-              done(null, {
-                id: user.id,
-                email: user.email,
+              createFacebookUser({
+                email: profile._json.email,
+                emailConfirmed: true,
+                logins: [{ name: loginName, key: profile.id }],
+                claims: [{ type: claimType, value: accessToken }],
               });
             }
           }
