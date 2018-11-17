85fd48cf916a312d5550142d0c12c603c04c22bc
sreym <sergeyreym@gmail.com>
2018-11-17 10:06:30 +0800

    add unit access rights

--- src/data/models/Unit.js
+++ src/data/models/Unit.js
@@ -1,5 +1,6 @@
 import DataType from 'sequelize';
 import Model from '../sequelize';
+import * as util from './util';
 
 const Unit = Model.define('unit', {
   id: {
@@ -21,5 +22,20 @@ const Unit = Model.define('unit', {
     type: DataType.STRING,
   },
 });
+
+Unit.prototype.canRead = async function canRead(user) {
+  if (util.haveAccess(user)) return true;
+  const courses = await this.getCourses();
+  for (let i = 0; i < courses.length; i += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    const role = await user.getRole(courses[i].id);
+    if (!role) return false;
+  }
+  return true;
+};
+
+Unit.prototype.canWrite = function canWrite(user) {
+  return util.haveRootAccess(user);
+};
 
 export default Unit;
