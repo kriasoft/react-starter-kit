e491acbf14b36fec000fc2713756ea7d8fa1a6ed
Mojtaba Izadmehr <m.eezadmehr@gmail.com>
2018-12-27 12:02:44 +0330

    jest.config: removed the file test stubs for static assets

--- jest.config.js
+++ jest.config.js
@@ -62,8 +62,6 @@ module.exports = {
   // A map from regular expressions to module names that allow to stub out resources,
   // like images or styles with a single module.
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/tools/mocks//staticAssetMock.js',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     'isomorphic-style-loader/lib/withStyles':
       '<rootDir>/tools/mocks/withStylesMock.js',
