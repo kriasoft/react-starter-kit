b19c4cbaed9893a8485d8dbc2a2e0f0a2704d9bf
piglovesyou <thepiglovesyou@gmail.com>
2019-09-01 15:26:29 +0900

    Update all deps by npm-check-updates
    * fix some TypeScript problems
    * disable some react related eslint rules
    * new huskey config in package.json

--- tools/runServer.ts
+++ tools/runServer.ts
@@ -34,8 +34,8 @@ function runServer(): Promise<ChildProcess> {
 
       if (match) {
         server.host = match[1];
-        server.stdout.removeListener('data', onStdOut);
-        server.stdout.on('data', x => process.stdout.write(x));
+        server.stdout!.removeListener('data', onStdOut);
+        server.stdout!.on('data', x => process.stdout.write(x));
         pending = false;
         resolve(server);
       }
@@ -46,7 +46,7 @@ function runServer(): Promise<ChildProcess> {
     }
 
     server = cp.spawn('node', [serverPath], {
-      env: Object.assign({ NODE_ENV: 'development' }, process.env),
+      env: { NODE_ENV: 'development', ...process.env },
     });
 
     if (pending) {
@@ -59,8 +59,8 @@ function runServer(): Promise<ChildProcess> {
       });
     }
 
-    server.stdout.on('data', onStdOut);
-    server.stderr.on('data', x => process.stderr.write(x));
+    server.stdout!.on('data', onStdOut);
+    server.stderr!.on('data', x => process.stderr.write(x));
 
     return server;
   });
