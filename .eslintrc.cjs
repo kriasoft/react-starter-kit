a0f7896fe2c5adecc4f596e1b02dc24de5051a8e
Nik Gushchin <game.nik.nik@gmail.com>
2022-06-29 17:28:34 +0300

    fix: ESLint override path for *.tsx files (#1973)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,7 +23,7 @@ module.exports = {
 
   overrides: [
     {
-      files: ["*.ts", ".tsx"],
+      files: ["*.ts", "*.tsx"],
       parser: "@typescript-eslint/parser",
       extends: ["plugin:@typescript-eslint/recommended"],
       plugins: ["@typescript-eslint"],
